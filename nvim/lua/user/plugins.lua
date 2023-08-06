2547bc456d9de62b13a89cea399353b04ae98899
Ilker Mutlu <ilker.mutlu@gmail.com>
2023-08-06 16:15:30 +0100

    Key mapping for toggling Floaterm

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -233,8 +233,8 @@ use({
     vim.g.floaterm_width = 0.8
     vim.g.floaterm_height = 0.8
     --vim.g.floaterm_wintype = 'split'
-    vim.keymap.set('n', '<F1>', ':FloatermToggle<CR>')
-    vim.keymap.set('t', '<F1>', '<C-\\><C-n>:FloatermToggle<CR>')
+    vim.keymap.set('n', '<Leader>tt', ':FloatermToggle<CR>')
+    vim.keymap.set('t', '<Leader>tt', '<C-\\><C-n>:FloatermToggle<CR>')
     vim.cmd([[
       highlight link Floaterm CursorLine
       highlight link FloatermBorder CursorLineBg
