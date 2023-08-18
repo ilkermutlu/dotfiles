49bc523ca090964b429ff35b9b8abb3e77bdd46f
Ilker Mutlu <ilker.mutlu@gmail.com>
2023-08-18 18:27:58 +0100

    Add key mapping for Telescope help_tags

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -67,6 +67,7 @@ vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
 vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', '<Leader>lp', ':Telescope help_tags<CR>')
 
 -- Commands
 vim.api.nvim_create_user_command('Format', vim.lsp.buf.format, {})
